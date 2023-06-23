5172c268c0b1c934bd490e0e70779ba6c091e5b3
Rickyandrian83 <100892760+Rickyandrian83@users.noreply.github.com>
2023-06-23 09:07:29 +0700

    Add files via upload

---
+++ website artikel/script.js
@@ -0,0 +1,7 @@
+const button = document.querySelector('button');
+const ricky = document.querySelector('#ricky');
+button.addEventListener('click', function() {
+	ricky.classList.toggle('dark');
+
+}  
+);
